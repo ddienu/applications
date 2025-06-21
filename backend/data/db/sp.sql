81b23d3089f9525e2b9b87adb72ee2fda4acc0fb
ddienu <rockdann@hotmail.com>
2025-06-21 01:42:15 -0500

    feat: Adding procedure to get all api users

--- backend/data/db/sp.sql
+++ backend/data/db/sp.sql
@@ -15,3 +15,14 @@ SELECT US.id,US.username,US.email,US.password_hash,US.status_id,UST.name AS stat
 INNER JOIN  user_status UST ON US.status_id=UST.id WHERE US.status_id=1 AND US.id=Id ORDER BY US.id;
 END $$
 DELIMITER ;
+
+DELIMITE $$
+DROP PROCEDURE IF EXISTS sp_get_api_users$$
+CREATE PROCEDURE sp_get_api_users()
+BEGIN
+SELECT * FROM user_role ur
+INNER JOIN user us ON ur.user_id = us.id
+INNER JOIN role rol ON rol.id = ur.role_id
+WHERE ur.role_id = 4;
+END $$
+DELIMITER ;
